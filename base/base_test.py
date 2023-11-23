99f98a494dbab2b150099f31189c9b9debd88360
vitali.dambajev <vitali.dambajev@gamesys.ee>
2023-11-23 11:35:09 +0200

    Added usability of logger in tests

--- base/base_test.py
+++ base/base_test.py
@@ -4,15 +4,19 @@ from pages.women_page import WomenPage
 from pages.men_page import MenPage
 from config.data import Data
 from config.locators_pytest_param import ClothColor, ClothSize, BaseLocators
+from config.logger import setup_logger
 
 
 class BaseTest:
+    LOGGER_MESSAGE = "FROM TEST ==>"
+
     women_page: WomenPage
     men_page: MenPage
     data: Data
     cloth_color: ClothColor
     cloth_size: ClothSize
     base_locators: BaseLocators
+    logger: setup_logger(__name__)
 
     @pytest.fixture(autouse=True)
     def setup(self, request, browser):
@@ -23,3 +27,4 @@ class BaseTest:
         request.cls.cloth_color = ClothColor()
         request.cls.cloth_size = ClothSize()
         request.cls.base_locators = BaseLocators()
+        request.cls.logger = setup_logger(self.LOGGER_MESSAGE)
